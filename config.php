9107a6887d56cb95f55c2850148794d2c38f473e
Nicole145 <37220316+Nicole145@users.noreply.github.com>
2020-08-03 10:50:02 +0100

    create a new analysis added

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+    $DBurl = 'http://www.aifdb.org';
+    $SSurl = 'http://www.arg-tech.org/~john/schemesets/list';
+    $CPurl = 'http://corpora.aifdb.org';
+    $TXurl = 'http://www.arg-tech.org/TXTdb';
