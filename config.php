1275684985527ad530dc78bf1d1daae14bdee6ee
Kamila Gorska <kgorska@dundee.ac.uk>
2021-11-02 16:00:08 +0000

    remove and ignore config.php file

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $DBurl = 'http://www.aifdb.org';
-    $SSurl = 'http://www.arg-tech.org/~john/schemesets/list';
-    $CPurl = 'http://corpora.aifdb.org';
-    $TXurl = 'http://www.arg-tech.org/TXTdb';
-?>
