f16e7e77fb51c272a13757ef045d2ac9e30d9005
Kamila Gorska <kgorska@dundee.ac.uk>
2021-11-03 14:12:51 +0000

    fixed broken link from analyse to index page

--- analyse.php
+++ analyse.php
@@ -169,7 +169,7 @@ if (isset($_COOKIE['ovauser'])) {
     <?php
     $newurl = "analyse.php?url=" . $_GET['url'] . $plusval;
     ?>
-    <a href="http://index.php" class="home"><img src="res/img/logo.svg" /></a>
+    <a href="index.php" class="home"><img src="res/img/logo.svg" /></a>
     <a onClick='$("#xmenu").toggle("slide", {direction: "right"}, "slow");' class="icon" id="xmenutoggle" style="background-position: -126px 50%;"></a>
     <div class="divider"></div>
     <a onClick="mainTut()" class="icon" style="background-position: -378px 50%;"><span class="tooltiptext">Tutorial</span></a>
